2355e42f16b69593ee670b52a6ffd4e67160cb0b
u.lakshmi92@gmail.com <l.pottumuthu@cv-library.co.uk>
2023-07-27 13:14:21 +0100

    changed folder structure for stepsdefinition files

--- config/cucumber.js
+++ config/cucumber.js
@@ -10,7 +10,7 @@ module.exports = {
         publishQuiet: true,
         dryRun: false,
         require: [
-            "src/test/stepDefinitions/Web/Candidate/*.ts",
+            "src/test/stepDefinitions/Web/*.ts",
             "src/hooks/hooks.ts"
         ],
         requireModule: [
